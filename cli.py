2261313c0bd198a6a0d66c1422cd8f222501b152
Bruno Hamel <bruno.hamel93@gmail.com>
2018-05-17 16:46:24 -0400

    Add files via upload

--- cli.py
+++ cli.py
@@ -2,36 +2,36 @@ import argparse
 
 import text_analyser
 
-def run():
 
-    parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser()
 
-    parser.add_argument("-f", "--french",
-                        help="French text to compare",
-                        type=str,
-                        required=True)
+parser.add_argument("-f", "--french",
+                    help="French text to compare",
+                    type=str,
+                    required=True)
 
-    parser.add_argument("-e", "--english",
-                        help="English text to compare",
-                        type=str,
-                        required=True)
+parser.add_argument("-e", "--english",
+                    help="English text to compare",
+                    type=str,
+                    required=True)
 
-    parser.add_argument("-a", "--algorithm",
-                        help="Similarity algorithm (default: cosinus)",
-                        type=str,
-                        choices=set(("cosinus", "jaccard")),
-                        default="cosinus")
+parser.add_argument("-a", "--algorithm",
+                    help="Similarity algorithm (default: cosinus)",
+                    type=str,
+                    choices=set(("cosinus", "jaccard")),
+                    default="cosinus")
 
-    args = parser.parse_args()
+args = parser.parse_args()
 
-    analyser = text_analyser.TextAnalyser()
+analyser = text_analyser.TextAnalyser()
 
-    algos = {
-        'cosinus': analyser.cos_similarity,
-        'jaccard':  analyser.jaccard_similarity
-    }
+algos = {
+    'cosinus': analyser.cos_similarity,
+    'jaccard':  analyser.jaccard_similarity
+    'levenshtein': analyser.levenshtein_similarity
+}
 
-    is_it = analyser.compare(
-        args.english, args.french, algos[args.algorithm])
+is_it = analyser.compare(
+    args.english, args.french, algos[args.algorithm])
 
-    print(is_it)
+print(is_it)
--- text_analyser.py
+++ text_analyser.py
@@ -1,53 +1,184 @@
 import string
+from statistics import mean
+from typing import Callable, List
 
 import nltk
 from nltk.corpus import stopwords
+from nltk.tokenize import ToktokTokenizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from textblob import TextBlob
 
 
 class TextAnalyser:
-    def _lem_normalize(self, text):
-        _remove_punct_dict = dict((ord(punct), None)
-                                  for punct in string.punctuation)
+    """
+    >>> t.compare('Hello', 'Bonjour', t.jaccard_similarity)
+    1.0
+    """
 
-        return self._lem_tokens(nltk.word_tokenize(
-            text.translate(_remove_punct_dict)))
+    def _lem_tokenize(self, text: str) -> List:
+        """
+        Tokenizes, lemmatize and removes punctuations from a string
 
-    def _lem_tokens(self, tokens):
+        Args:
+            text (str): The text to process.
+
+        Returns:
+           list: Tokens outputed from processes.
+
+        >>> t._lem_tokenize('many cars are on the roads.')
+        ['many', 'car', 'are', 'on', 'the', 'road']
+        """
+
         lemmer = nltk.stem.WordNetLemmatizer()
-        return [lemmer.lemmatize(token) for token in tokens]
+        table = str.maketrans({key: None for key in string.punctuation})
+        text = text.translate(table)
+        tokens = ToktokTokenizer().tokenize(text)
 
-    def _translate_to(self, text, lang='en'):
+        tokenized = [lemmer.lemmatize(token) for token in tokens]
+
+        return tokenized
+
+    def _translate_to(self, text: str, lang: str = 'en') -> str:
+        """
+        Translates string to a language.
+
+        Args:
+            text (str): Text to translate.
+            lang (str): language to translate into. Defaults to 'en'.
+
+        Returns:
+           str: Translated text.
+
+        >>> t._translate_to('Bonjour, je suis un programme')
+        'Hello, I am a program'
+
+        >>> t._translate_to('Hello, I am a program', 'fr')
+        'Bonjour, je suis un programme'
+
+        """
         return str(TextBlob(text).translate(to=lang))
 
-    def _pre_process(self, text, lang='en'):
+    def _pre_process(self, text: str, lang: str='en') -> str:
+        """
+        Pre process string before yb removing stop words and lowering words.
+
+        Args:
+            text (str): Text to process.
+            lang (str): Language of the text. Defaults to 'en'.
+
+        Returns:
+           str: Processed text.
+
+        >>> t._pre_process('This is for testing')
+        'this testing'
+        """
+
         if lang == 'en':
             lang = 'english'
         else:
             lang = 'french'
 
         blob = TextBlob(text)
-        words = [word.lemmatize().lower()
+        words = [word.lower()
                  for word in blob.words if word not in stopwords.words(lang)]
 
         return ' '.join(words)
 
-    def cos_similarity(self, text_fr, text_en):
+    def cos_similarity(self, text_fr: str, text_en: str) -> float:
+        """
+        Cosine similarity calculates the cosinus angle
+        between the two vector of words.
+
+        Args:
+            text_en (str): English text.
+            text_fr (str): French text.
+
+        Returns:
+           float: Cosine similarity.
+
+        >>> t.cos_similarity('Hello', 'Hello')
+        1.0
+        """
         text_list = [text_fr, text_en]
 
-        TfidfVec = TfidfVectorizer(tokenizer=self._lem_normalize)
+        TfidfVec = TfidfVectorizer(tokenizer=self._lem_tokenize)
 
         tfidf = TfidfVec.fit_transform(text_list)
         return (tfidf * tfidf.T).toarray()[0][1]
 
-    def jaccard_similarity(self, text_fr, text_en):
+    def jaccard_similarity(self, text_fr: str, text_en: str) -> float:
+        """
+        Jaccard similarity is a statistic used for comparing
+        the similarity and diversity of sample sets. It measures
+        similarity between finite sample sets, and is defined
+        as the size of the intersection divided by the size of
+        the union of the sample sets.
+
+        Args:
+            text_en (str): English text.
+            text_fr (str): French text.
+
+        Returns:
+           float: Jaccard similarity.
+
+        >>> t.jaccard_similarity('Hello', 'Hello')
+        1.0
+        """
+
         a = set(text_fr.split())
         b = set(text_en.split())
         c = a.intersection(b)
         return float(len(c)) / (len(a) + len(b) - len(c))
 
-    def compare(self, text_en, text_fr, similarity_metric):
+    def levenshtein_similarity(self, text_fr: str, text_en: str) -> float:
+        """
+        Levenshtein calculates the number of changes
+        that are needed to go from a string to another.
+
+        Args:
+            text_en (str): English text.
+            text_fr (str): French text.
+
+        Returns:
+           float: 1 - (number of changes / length of string).
+
+        >>> t.levenshtein_similarity("Programming is cool", "Programming is awsome")
+        0.75
+        """
+        if len(text_fr) < len(text_en):
+            return self.levenshtein(text_en, text_fr)
+
+        if len(text_en) == 0:
+            return len(text_fr)
+
+        previous_row = range(len(text_en) + 1)
+        for i, c1 in enumerate(text_fr):
+            current_row = [i + 1]
+            for j, c2 in enumerate(text_en):
+                insertions = previous_row[j + 1] + 1
+                deletions = current_row[j] + 1
+                substitutions = previous_row[j] + (c1 != c2)
+                current_row.append(min(insertions, deletions, substitutions))
+            previous_row = current_row
+
+        return 1 - (previous_row[-1] / mean([len(text_fr), len(text_en)]))
+
+    def compare(self, text_en: str, text_fr: str,
+                similarity_metric: Callable) -> float:
+        """
+        Compare two string with a similarity algorithm.
+
+        Args:
+            text_en (str): English text.
+            text_fr (str): French text.
+            similarity_metric (Callable): Similarity algorithm.
+
+        Returns:
+           float: The similarity metric.
+
+        >>> t.compare('Hello', 'Bonjour', t.cos_similarity)
+        1.0
+        """
         translated_fr = self._translate_to(text_fr, 'en')
 
         processed_en = self._pre_process(text_en)
@@ -57,5 +188,25 @@ class TextAnalyser:
 
         return sim
 
-    def is_same(self, text_en, text_fr, threshold=0.29):
-        return self.compare(text_en, text_fr) > threshold
+    def is_same(self, text_en: str, text_fr: str,
+                threshold: float=0.29) -> float:
+        """
+        Check if two strings are the same.
+
+        Args:
+            text_en (str): English text.
+            text_fr (str): French text.
+            threshold (float): Cosine similarity value considered high enough.
+
+        Returns:
+            bool: If the two strings are the same according to the threshold.
+
+        >>> t.is_same('Hello', 'Bonjour')
+        True
+        """
+        return self.compare(text_en, text_fr, self.cos_similarity) > threshold
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(extraglobs={'t': TextAnalyser()})
